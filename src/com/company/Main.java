fd2eb955613644d12efc0293766b5ce8e05aa0fe
Hung <nguyenkhachung0708@gmail.com>
2022-03-22 22:18:21 +0700

    demo

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+
+        System.out.println("Please enter your weight!!!");
+        weight = scanner.nextDouble();
+
+        System.out.println("Please enter your height!!!");
+        height = scanner.nextDouble();
+        bmi = weight / Math.pow(height, 2);
+
+        System.out.printf("%-9s%s", "bmi", "Interpretation\n");
+        if (bmi < 18)
+            System.out.printf("%-9.2f%s", bmi, "Underweight");
+        else if (bmi < 25.0)
+            System.out.printf("%-9.2f%s", bmi, "Normal");
+        else if (bmi < 30.0)
+            System.out.printf("%-9.2f%s", bmi, "Overweight");
+        else
+            System.out.printf("%-9.2f%s", bmi, "Obese");
+
+    }
+}
